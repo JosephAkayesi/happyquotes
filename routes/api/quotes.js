f8c285f35928906907928e5be5863b8fa2a18cce
Joseph Akayesi <josephakayesi@gmail.com>
2019-02-07 22:54:44 +0000

    Added delete quote route

--- routes/api/quotes.js
+++ routes/api/quotes.js
@@ -5,6 +5,9 @@ const passport = require('passport')
 //Load Quote model
 const Quote = require('../../models/Quote')
 
+//Load Admin model
+const Admin =  require('../../models/Admin')
+
 //Validation
 const validateQuoteInput = require('../../validation/quote')
 
@@ -20,7 +23,7 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
     const { errors, isValid } = validateQuoteInput(req.body)
 
     //Check validation
-    if(!isValid){
+    if (!isValid) {
         return res.status(400).json(errors)
     }
 
@@ -31,9 +34,48 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
         dateAdded: Date.now(),
         lastModified: Date.now()
     })
-    
+
     console.warn(newQuote)
     newQuote.save().then(post => res.json(post))
+})
+
+// @route   GET api/quotes
+// @desc    Get quotes
+// @access  Public
+router.get('/', (req, res) => {
+    Quote.find()
+        .sort({ dateAdded: -1 })
+        .then(quotes => res.json(quotes))
+        .catch(err => res.status(404).json({ noQuotesFound: 'No Quote found with that Id' }))
+})
+
+// @route   GET api/quotes/:id
+// @desc    Get quotes by id
+// @access  Public
+router.get('/:id', (req, res) => {
+    Quote.findById(req.params.id)
+        .then(quote => res.json(quote))
+        .catch(err => res.status(404).json({ noQuoteFound: 'No quote found with that Id' }))
+})
+
+// @route   DELETE api/quotes/:id
+// @desc    Delete quote
+// @access  Public
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    // console.log(req.user.id)
+    Admin.findOne({admin: req.user.id})
+        .then(admin => {
+            Quote.findById(req.params.id)
+                .then(quote => {
+                    if(quote.admin.toString() !== req.user.id){
+                        return res.status(401).json({notAuthorized: 'Admin not authorized'})
+                    }
+                    quote.remove()
+                        .then(() => res.json({success: true}))
+                })
+                .catch(err => res.status(404).json({noQuoteFound: 'No quote found'}))
+        })
+        
 })
 
 module.exports = router;
