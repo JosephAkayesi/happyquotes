5103e5bde8ed4e304936077e379c652c2d105651
Joseph Akayesi <josephakayesi@gmail.com>
2019-03-17 19:12:33 +0000

    Quotes data dynamic. Able to fetch quotes from DB and randomise

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import Quotes from '../Quotes/Quotes'
 import Footer from '../Footer/Footer'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { getQuotes } from '../../actions/quoteActions'
 import './Layout.css'
 
 class Layout extends Component {
+    componentDidMount() {
+        this.props.getQuotes()
+        // this.setFirstQuote()
+    }
+
     render() {
+        const { quotes } = this.props.quote
+        
         return (
             <div className='bgImg'>
                 <div className='centered text-center align-items-center justify-content-center'>
-                    <Quotes />
+                    <Quotes quotes={quotes}/>
                 </div>
                 <Footer />
             </div>
@@ -16,4 +26,12 @@ class Layout extends Component {
     }
 }
 
-export default Layout
+Layout.propTypes = {
+    quote: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+    quote: state.quote,
+})
+
+export default connect(mapStateToProps, { getQuotes })(Layout)
--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react'
 import Pattern from '../Pattern/Pattern'
+import Spinner from '../Spinner/Spinner'
 
 class Quotes extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            quotes: [
-                {
-                    "quote": "Our industry does not respect tradition - it only respects innovation.",
-                    "author": "Satya Nadella",
-                    "position": "CEO of Microsoft",
-                    "admin": "Joseph Akayesi"
-                },
-                {
-                    "quote": "Engineering is the closest thing to magic that exists in the world.",
-                    "author": "Elon Musk",
-                    "position": "Tesla and SpaceX CEO",
-                    "admin": "Joseph Akayesi" 
-                },
-                {
-                    "quote": "For me, it matters that we drive technology as an equalizing force, as an enabler for everyone around the world.",
-                    "author": "Sundar Pichai",
-                    "position": "CEO of Google",
-                    "admin": "Yasmine Adams",
-                }
-            ],
-
+            quotes: [],
             currentQuote: {}
         }
     }
@@ -37,23 +18,37 @@ class Quotes extends Component {
     }
 
     randomiseQuotes = () => {
-        setTimeout(() => {
+        setInterval(() => {
             let index = Math.floor(Math.random() * (this.state.quotes.length))
             this.setState({ currentQuote: this.state.quotes[index] })
         }, 5000)
     }
 
-    componentDidMount(){
+    componentDidMount() {
+        this.randomiseQuotes();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('receiver props')
+        if (nextProps.quotes) {
+            this.setState({ quotes: nextProps.quotes })
+        }
         this.setFirstQuote()
     }
 
     render() {
-        this.randomiseQuotes()
         return (
             <div>
-                <h1 className='quote' style={{ color: 'black' }}>{this.state.currentQuote.quote}</h1>
-                <p style={{ color: 'black' }} ><em>- {this.state.currentQuote.author}</em></p>
-                <Pattern />
+                {this.state.currentQuote ?
+                    <div>
+                        <h1 className='quote' style={{ color: 'black' }}>{this.state.currentQuote.quote}</h1>
+                        <p style={{ color: 'black' }} ><em>- {this.state.currentQuote.author}</em></p>
+                        <Pattern />
+                    </div>
+                    :
+                    <Spinner />
+                }
+
             </div>
         )
     }
