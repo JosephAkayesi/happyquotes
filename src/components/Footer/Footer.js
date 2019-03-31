53475910c67417083bc89437a0483a344046a1d2
Joseph Akayesi <josephakayesi@gmail.com>
2019-03-31 09:42:19 +0000

    Fixed Footer spacing issue

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <p className='text-center' style={{ bottom: 0, color: 'black', width: '100%', position: 'fixed', display: 'block', zIndex: 100 }}>&copy; {new Date().getUTCFullYear()} &nbsp; | &nbsp;<Link to='/contributors' href='#contributors'>Contributors</Link></p>
+        <p className='text-center' style={{ bottom: 0, color: 'black', width: '100%', position: 'fixed', display: 'block', zIndex: 100 }}>&copy; {new Date().getUTCFullYear()}&nbsp; | &nbsp;<Link to='/contributors' href='#contributors'>Contributors</Link></p>
     )
 }
 
