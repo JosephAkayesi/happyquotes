3b2e2823389d175983b93272826e289a60e7eea8
Joseph Akayesi <josephakayesi@gmail.com>
2019-06-01 06:41:16 +0000

    Change paths in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
       <Provider store={store}>
         <BrowserRouter>
           <div>
-            <Route exact path='/' component={Layout} />
+            <Route path='/' component={Layout} />
             <Route exact path='/admin' component={Admin} />
             <Route exact path='/contributors' component={Contributors} />
           </div>
